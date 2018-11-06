1a79a16677f6203a9249565c8ac0c137a6ec07cf
Chester Swann-Auger <s6089488@tees.ac.uk>
2018-11-06 11:46:54 +0000

    GuestBooking views added, customers can be booked on event

--- ThAmCo.Events/Controllers/EventsController.cs
+++ ThAmCo.Events/Controllers/EventsController.cs
@@ -24,6 +24,8 @@ namespace ThAmCo.Events.Controllers
             return View(await _context.Events.ToListAsync());
         }
 
+
+
         // GET: Events/Details/5
         public async Task<IActionResult> Details(int? id)
         {
